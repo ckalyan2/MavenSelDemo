933a322313b8dc1b18872390589fb7ec629db416
ckalyan2 <31881827+ckalyan2@users.noreply.github.com>
2018-11-05 16:29:49 +0530

    First Commit

---
+++ Test/src/main/java/erecruitTestSample/Test/App.java
@@ -0,0 +1,24 @@
+package erecruitTestSample.Test;
+
+import java.util.concurrent.TimeUnit;
+
+import erecruitTestSample.Test.BaseClass;
+import erecruitTestSample.Test.CandidateCreation;
+
+public class App extends BaseClass {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+
+		BaseClass BC = new BaseClass();
+		CandidateCreation CC = new CandidateCreation();
+		BC.LoginPage();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		//CC.FlightSearch();
+		//CC.StudentDiscount();
+		BC.AppLoin();
+		BC.AddCandidatewithoutResume();
+		//BC.AddCandidatewithResume();
+		//BC.CloseApp();
+	}
+}
