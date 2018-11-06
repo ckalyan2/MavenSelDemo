5b940844339a4b229f052f81ed70c5c0b440dbdc
ckalyan2 <31881827+ckalyan2@users.noreply.github.com>
2018-11-06 17:40:36 +0530

    Latest COde

--- Test/src/main/java/erecruitTestSample/Test/BaseClass.java
+++ Test/src/main/java/erecruitTestSample/Test/BaseClass.java
@@ -47,7 +47,7 @@ public class BaseClass {
 	{
 		driver.findElement(By.xpath("//img[@src=\"/App_Themes/Default/images/logos/logo.png\"]")).click();
 		driver.findElement(By.xpath("//li[@class=\"new\"]//span[@data-bind=\"text: Title\"]")).click();
-//		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		//action.moveToElement(element).perform();
 		WebElement element = driver.findElement(By.xpath("//li[@class=\"candidate\"]//span[@title=\"Keyboard Shortcut: Shift+N, C\"]"));
 		Actions action = new Actions(driver);
@@ -81,8 +81,8 @@ public class BaseClass {
 			}
 		}
 //Bootstrap implementation ends.
-		driver.findElement(By.xpath("//input[@id=\"ctl00_cphMain_Address_txtFName\"]")).sendKeys("KalSelfrstTest18");
-		driver.findElement(By.xpath("//input[@id=\"ctl00_cphMain_Address_txtLName\"]")).sendKeys("KalSellastTest18");
+		driver.findElement(By.xpath("//input[@id=\"ctl00_cphMain_Address_txtFName\"]")).sendKeys("KalSelfrstTest19");
+		driver.findElement(By.xpath("//input[@id=\"ctl00_cphMain_Address_txtLName\"]")).sendKeys("KalSellastTest19");
 		driver.findElement(By.xpath("//input[@id=\"ctl00_cphMain_Address_txtAddr1\"]")).sendKeys("Miyapur,HYDERABAD");
 		driver.findElement(By.xpath("//input[@id=\"AdSourceDropdown_ddladsource\"]")).click();
 		driver.findElement(By.xpath("//ul/li/a[text()=\"Broadlook\"]")).click();
@@ -124,13 +124,13 @@ public class BaseClass {
 	public void AddCompany()
 	{
 		driver.findElement(By.xpath("//img[@src=\"/App_Themes/Default/images/logos/logo.png\"]")).click();
-		driver.findElement(By.xpath("//li[@class=\"new\"]//span[@data-bind=\"text: Title\"]")).click();
+		driver.findElement(By.xpath("//span[contains(text(),\\\"Add New\\\")]")).click();
 //		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		//action.moveToElement(element).perform();
 		driver.findElement(By.xpath("//li[@class=\"company\"]//span[@title=\"Keyboard Shortcut: Shift+N, M\"]")).click();
 		WebElement ComFrame = driver.findElement(By.xpath("//iframe[contains(@id,'company_new')]"));
 		driver.switchTo().frame(ComFrame);
-		driver.findElement(By.xpath("//input[@id='ctl00_cphMain_ctlAddress_txtCompany']")).sendKeys("VamSelTestCompanyauto1");
+		driver.findElement(By.xpath("//input[@id='ctl00_cphMain_ctlAddress_txtCompany']")).sendKeys("VamSelTestCompanyauto3");
 		WebElement ComFolgrp = driver.findElement(By.xpath("//input[@id=\"ctl00_cphMain_ddlFolderGroups_Input\"]"));
 		ComFolgrp.sendKeys("Acct/Finance | Acct/ Finance | A/P - A/R");
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
@@ -139,7 +139,7 @@ public class BaseClass {
 		WebElement ComSource1 = driver.findElement(By.xpath("//ul/li/a[text()='Broadlook']"));
 		ComSource1.click();
 		driver.findElement(By.xpath("//input[@value='Select a Communication Type' and @id='ctl00_cphMain_ucCommunicationMethods_rptEdit_ctl01_ddlType_Input']")).sendKeys("Home Phone");
-		driver.findElement(By.xpath("//input[@id='ctl00_cphMain_ucCommunicationMethods_rptEdit_ctl01_txtValue' and @class='riTextBox riEnabled txt']")).sendKeys("8985726867");
+		driver.findElement(By.xpath("//input[@id='ctl00_cphMain_ucCommunicationMethods_rptEdit_ctl01_txtValue' and @class='riTextBox riEnabled txt']")).sendKeys("9845457547");
 		/*		List<WebElement> lists1 = driver.findElements(By.xpath("//div[@id=\"ctl00_cphMain_ucCommunicationMethods_rptEdit_ctl02_ddlType_DropDown\"]//div[@class=\"rcbScroll rcbWidth\"]//ul[@class=\"rcbList\" and @style=\"list-style:none;margin:0;padding:0;zoom:1;\"]/li"));
 		System.out.println(lists1.size());
 		for (int i=0; i<=lists1.size(); i++)
